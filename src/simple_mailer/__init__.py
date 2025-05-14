6ad4e06e77dc615aa066f32c252482cb8681e49e
Jonathan Shaya <jonathansh2@mta.ac.il>
2025-05-14 21:54:53 +0300

    -added custom errors to init

--- src/simple_mailer/__init__.py
+++ src/simple_mailer/__init__.py
@@ -6,11 +6,11 @@ A simple Python library to send email confirmations via Gmail using App Password
 featuring a unique passcode and a timed confirmation link.
 """
 
-from .passcode_link_mailer import PasscodeLinkMailer
+from .passcode_link_mailer import PasscodeLinkMailer, EmailSendingError, EmailSendingAuthError, EmailSendingConnectionError
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Jonatan Shaya"
 __email__ = "Jonatan.shaya99@gmail.com"
 
 
-__all__ = ['PasscodeLinkMailer']
+__all__ = ['PasscodeLinkMailer', 'EmailSendingError', 'EmailSendingAuthError', 'EmailSendingConnectionError']
