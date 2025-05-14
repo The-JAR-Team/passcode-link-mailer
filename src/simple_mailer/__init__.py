0ddc0fd9bc1c06facf515f363ba2dc8b026ca3ea
Jonathan Shaya <jonathansh2@mta.ac.il>
2025-05-14 21:19:53 +0300

    -updated version

--- src/simple_mailer/__init__.py
+++ src/simple_mailer/__init__.py
@@ -8,7 +8,7 @@ featuring a unique passcode and a timed confirmation link.
 
 from .passcode_link_mailer import PasscodeLinkMailer
 
-__version__ = "0.1.2"
+__version__ = "0.1.1"
 __author__ = "Jonatan Shaya"
 __email__ = "Jonatan.shaya99@gmail.com"
 
